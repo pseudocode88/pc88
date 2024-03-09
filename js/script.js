41dfb21d5dc8d77515026c25aba7af6f85c47426
pseudocode88 <psuedocode88@protonmail.com>
2024-03-09 16:03:11 +0700

    Add music on knot click

--- js/script.js
+++ js/script.js
@@ -1,52 +1,22 @@
 var $e = {};
-var player;
-
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-function onYouTubeIframeAPIReady() {
-    console.log('yt ready');
-
-    player = new YT.Player('player', {
-        height: '1',
-        width: '1',
-        videoId: '',
-        events: {
-            'onReady': onPlayerReady
-            // 'onStateChange': onPlayerStateChange
-        }
-    });
-
-    init();
-}
-
-function onPlayerReady() {
-    player.loadVideoById("Z1sSqyeEg5o", 0, "large")
-    player.pauseVideo();
-
-}
-
-var done = false;
-// function onPlayerStateChange(event) {
-//     if (event.data == YT.PlayerState.PLAYING && !done) {
-//         setTimeout(stopVideo, 6000);
-//         done = true;
-//     }
-// }
 
 function init() {
     $e.knot = document.getElementById('knot');
-    $e.player = document.getElementById('musicplayer');
+    $e.player = document.getElementById('player');
 
-    $e.knot.addEventListener('mouseenter', function () {
-        player.playVideo();
+    $e.knot.addEventListener('click', function () {
+        $e.player.play();
+        // $e.knot.className = 'knot-rotate';
+
+        $e.knot.setAttribute('class', "knot-rotate")
+
+        console.log(this, $e.knot.className)
     })
 
-    $e.knot.addEventListener('mouseleave', function () {
-        player.stopVideo();
-    })
+    // $e.knot.addEventListener('mouseleave', function () {
+    //     $e.player.pause();
+    //     $e.player.currentTime = 0;
+    // })
 }
 
-// window.onload = init;
+window.onload = init;
