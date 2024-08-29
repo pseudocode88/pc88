b365d61973975990a4e1a6d92139ebbe5276e8fa
pseudocode88 <psuedocode88@protonmail.com>
2024-08-29 20:34:13 +0700

    Add clouds and animation

--- js/script.js
+++ js/script.js
@@ -8,11 +8,17 @@ function init() {
         $e.player.play();
         $e.knot.setAttribute('class', "knot-rotate")
         document.body.className = 'turn-body';
+        document.getElementById('cloud-left').className = 'cloud-left-up';
+        document.getElementById('cloud-right').className = 'cloud-right-up';
+        document.getElementById('dark-sky').className = 'dark-sky-up';
     })
 
     $e.player.addEventListener('ended', function () {
         $e.knot.setAttribute('class', "knot-rotate-back")
         document.body.className = '';
+        document.getElementById('cloud-left').className = 'cloud-left-down';
+        document.getElementById('cloud-right').className = 'cloud-right-down';
+        document.getElementById('dark-sky').className = 'dark-sky-down';
     })
 }
 
